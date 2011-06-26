1f8d7cbb0d431a378796a5d65cdc9d59013066ee
Felix Geisendörfer <felix@debuggable.com>
2011-06-26 17:05:02 +0200

    local / global injection

---
+++ test/integration/test-global-injection.js
@@ -0,0 +1,12 @@
+var common = require('../common');
+var assert = common.assert;
+var SandboxedModule = require(common.dir.lib + '/sandboxed_module');
+
+(function testGlobalInjection() {
+  var fakeProcess = {my: 'my process'};
+  var globals = SandboxedModule.load(common.dir.fixture + '/global', {
+    global: {process: fakeProcess},
+  }).exports;
+
+  assert.strictEqual(globals.process, fakeProcess);
+})();
---
+++ test/integration/test-local-injection.js
@@ -0,0 +1,11 @@
+var common = require('../common');
+var assert = common.assert;
+var SandboxedModule = require(common.dir.lib + '/sandboxed_module');
+
+(function testLocalInjection() {
+  var locals = SandboxedModule.load(common.dir.fixture + '/local', {
+    local: {__filename: 'my filename'},
+  }).exports;
+
+  assert.strictEqual(locals.__filename, 'my filename');
+})();
