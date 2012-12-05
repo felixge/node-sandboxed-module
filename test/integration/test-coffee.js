8369378bb9944301f07cc8d3f183b549fb326976
Domenic Denicola <domenic@domenicdenicola.com>
2012-12-05 03:44:33 -0500

    Fix CoffeeScript test.
    You need to `require('coffee-script')` before requiring CoffeeScript modules.

--- test/integration/test-coffee.js
+++ test/integration/test-coffee.js
@@ -3,8 +3,14 @@ var assert = common.assert;
 var SandboxedModule = require(common.dir.lib + '/sandboxed_module');
 
 (function testRequire() {
-  var test = require('../fixture/coffeeClass');
-  console.log(new test().simpleData());
-  var coffeeClass = SandboxedModule.load('../fixture/coffeeClass').exports;
-  assert.strictEqual(new coffeeClass().simpleData(), 2);
+  var hasCoffee = false;
+  try {
+    require('coffee-script');
+    hasCoffee = true;
+  } catch (e) {}
+
+  if (hasCoffee) {
+    var coffeeClass = SandboxedModule.load('../fixture/coffeeClass').exports;
+    assert.strictEqual(new coffeeClass().simpleData(), 2);
+  }
 })();
