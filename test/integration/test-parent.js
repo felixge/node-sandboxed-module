295ab846c81eadfcd4f25fe24fdb311c8acfb746
oroce <robert@oroszi.net>
2013-09-05 19:06:54 +0200

    parent.filename should equal sandboxed-module

--- test/integration/test-parent.js
+++ test/integration/test-parent.js
@@ -2,5 +2,5 @@ var assert = require('assert');
 var SandboxedModule = require('../..');
 
 var exports = SandboxedModule.load('../fixture/parent');
-console.log("exports", exports)
 assert.ok(exports.module.parent);
+assert.equal(exports.module.parent.filename, require.resolve('../..'));
