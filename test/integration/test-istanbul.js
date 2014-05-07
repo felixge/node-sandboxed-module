93fce6323356b1c255e1cfeffc03ba41afe5e81e
Rob Richardson <robrich@robrich.org>
2014-05-07 14:40:12 -0700

    Fix for using Istanbul programmatically.
    Use both the '$$cov_*' and istanbul's default '__coverage__' var to see if istanbul is running

--- test/integration/test-istanbul.js
+++ test/integration/test-istanbul.js
@@ -2,10 +2,15 @@ var assert = require('assert');
 var SandboxedModule = require('../..');
 SandboxedModule.registerBuiltInSourceTransformer('istanbul');
 
-global['$$cov_1234'] = {};
-var baz = SandboxedModule.load('../fixture/baz').exports,
-    instrumentedFunction = /^function \(\){__cov_.*\.f\['1'\]\+\+;__cov_.*\.s\['2'\]\+\+;return 1\+3;}$/;
+function testIt(coverageVariable) {
+    global[coverageVariable] = {};
+    var baz = SandboxedModule.load('../fixture/baz').exports,
+        instrumentedFunction = /^function \(\){__cov_.*\.f\['1'\]\+\+;__cov_.*\.s\['2'\]\+\+;return 1\+3;}$/;
 
-assert.strictEqual(baz.biz.toString().match(instrumentedFunction).length, 1);
+    assert.strictEqual(baz.biz.toString().match(instrumentedFunction).length, 1);
 
-delete global['$$cov_1234'];
+    delete global[coverageVariable];
+}
+
+testIt('$$cov_1234');
+testIt('__coverage__');
