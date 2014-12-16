14c67183d6e0394fc55c8564596bd2687d06addb
mminder <marvinminder@gmail.com>
2014-12-16 10:59:36 +0100

    #41 added test

---
+++ test/integration/test-recursive-source-transformer.js
@@ -0,0 +1,15 @@
+var assert = require('assert');
+var SandboxedModule = require('../..');
+
+var requireModule = SandboxedModule.load('../fixture/require', {
+  sourceTransformers: {
+    turnBarToReplacedBar: function (source) {
+      return source.replace(/module.exports = 'bar';/g, 'module.exports = \'replacedBar\'');
+    }
+  },
+  applySourceTransformersRecursively: true
+});
+
+var recursiveExports = requireModule.exports;
+assert.strictEqual(recursiveExports.bar, 'replacedBar');
+assert.strictEqual(recursiveExports.foo.bar, 'replacedBar');
