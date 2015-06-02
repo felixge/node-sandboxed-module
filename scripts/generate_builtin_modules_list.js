6a92f7dee7c9a65251903cdbd4fe516311a0a3af
Domenic Denicola <d@domenic.me>
2015-06-02 18:57:01 -0400

    Update builtin modules script for io.js
    Since the node.js is now in strict mode we can't get into it with `caller`. However we can use more straightforward hacks.

--- scripts/generate_builtin_modules_list.js
+++ scripts/generate_builtin_modules_list.js
@@ -1,22 +1,5 @@
-// Stay in sloppy mode since we need `.caller` :(
+'use strict';
 var fs = require('fs');
 var path = require('path');
 
-// Based on https://gist.github.com/Benvie/1841241
-
-var NativeModule;
-
-// Intercept NativeModule.require's call to process.moduleLoadList.push
-process.moduleLoadList.push = function newPush() {
-  // `NativeModule.require('native_module')` returns NativeModule
-  NativeModule = newPush.caller('native_module');
-
-  // Delete the interceptor and forward normal functionality
-  delete process.moduleLoadList.push;
-  return Array.prototype.push.apply(process.moduleLoadList, arguments);
-}
-
-// Force an initial call to the interceptor
-require('vm');
-
-fs.writeFileSync(path.resolve(__dirname, '../lib/builtin_modules.json'), JSON.stringify(Object.keys(NativeModule._source), undefined, 2));
+fs.writeFileSync(path.resolve(__dirname, '../lib/builtin_modules.json'), JSON.stringify(Object.keys(process.binding('natives')), undefined, 2));
