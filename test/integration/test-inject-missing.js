2bf19d015418a34d0a90c8b707ce059481052761
aetherwalker <aetherwalker@refugesystems.net>
2017-11-20 01:34:48 -0500

    Proposed solution for #64

--- test/integration/test-inject-missing.js
+++ test/integration/test-inject-missing.js
@@ -5,6 +5,5 @@ var foo = SandboxedModule.require('../fixture/foo', {
 	"requires": {"doesNotExist": {}},
 	"ignoreMissing": true
 });
-
 assert.strictEqual(foo.foo, 'foo');
 assert.strictEqual(foo.bar, 'bar');
