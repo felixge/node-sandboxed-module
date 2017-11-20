2f6a8c807642000541f0264a44744102c99cae70
aetherwalker <aetherwalker@refugesystems.net>
2017-11-20 01:28:24 -0500

    Adding test case for ignoreMissing option

---
+++ test/integration/test-inject-missing.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+var SandboxedModule = require('../..');
+
+var foo = SandboxedModule.require('../fixture/foo', {
+	"requires": {"doesNotExist": {}},
+	"ignoreMissing": true
+});
+
+assert.strictEqual(foo.foo, 'foo');
+assert.strictEqual(foo.bar, 'bar');
